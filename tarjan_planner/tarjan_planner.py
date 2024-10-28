64fa6b6a6209cc5df15542ec15e70374b2d31f86
Christopher Kaba <chris.kaba@gmail.com>
2024-10-28 11:50:46 +0100

    foggy, get plot to work

--- tarjan_planner/tarjan_planner.py
+++ tarjan_planner/tarjan_planner.py
@@ -11,10 +11,16 @@ from .transport_manager import TransportManager
 relatives_manager = RelativesManager("tarjan_planner/relatives.csv")
 transport_manager = TransportManager("tarjan_planner/transport_modes.csv")
 
-route_map = nx.Graph()
+def draw_nodes():
+    route_map = nx.Graph()
 
-for relative in relatives_manager.get_relatives():
-    route_map.add_node(
-        relative["Relative"],
-        pos=(relative["Longitude"], relative["Latitude"])
-    )
+    for relative in relatives_manager.get_relatives():
+        route_map.add_node(
+            relative["Relative"],
+            pos=(relative["Longitude"], relative["Latitude"])
+        )
+
+    pos = nx.get_node_attributes(route_map, 'pos')
+    nx.draw(route_map, pos, with_labels=True)
+
+    plt.show()
